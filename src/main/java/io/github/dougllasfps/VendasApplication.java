b861768674e805eca8829804adaf43bc9767ca19
Sergio Prado <sergio453@gmail.com>
2022-10-05 17:01:24 -0300

    Novo commit

--- src/main/java/io/github/dougllasfps/VendasApplication.java
+++ src/main/java/io/github/dougllasfps/VendasApplication.java
@@ -1,11 +1,24 @@
 package io.github.dougllasfps;
 
+import io.github.dougllasfps.domain.entity.Cliente;
+import io.github.dougllasfps.domain.repositorio.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VendasApplication {
 
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes){
+        return args -> {
+            Cliente cliente = new Cliente();
+            cliente.setNome("Douglas");
+            clientes.salvar(cliente);
+        };
+    }
 
     public static void main(String[] args){
 
---
+++ src/main/java/io/github/dougllasfps/domain/repositorio/Clientes.java
@@ -0,0 +1,20 @@
+package io.github.dougllasfps.domain.repositorio;
+
+import io.github.dougllasfps.domain.entity.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class Clientes {
+
+    private static String INSERT = "insert into Cliente(nome) values(?) ";
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public Cliente salvar(Cliente cliente){
+        jdbcTemplate.update(INSERT, new Object[]{cliente.getNome()});
+        return cliente;
+    }
+}
